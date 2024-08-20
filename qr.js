e7e479d255cdd5e0d5526833aed427f14bd35a67
FredieTech <frediezra60@gmail.com>
2024-08-21 02:32:47 +0300

    Update qr.js

--- qr.js
+++ qr.js
@@ -59,34 +59,34 @@ router.get('/', async (req, res) => {
 				   let session = await Qr_Code_By_Maher_Zubair.sendMessage(Qr_Code_By_Maher_Zubair.user.id, { text: '' + b64data });
 	
 				   let SIGMA_MD_TEXT = `
-*LUCKY MD V5 QR CODE CONNECTED*
+*KENTECH MD QR CODE CONNECTED*
 
-> You have completed the first step of Deploying *LUCKY_MD V5*, Copy the above message and deploy your bot.Meanwhile you must follow the 4 steps. 
+> You have completed the first step of Deploying *KENTEXH_MD*, Copy the above message and deploy your bot.Meanwhile you must follow the 4 steps. 
 
 > 1st STEP
 *TAP THE LINK BELOW AND FOLLOW THE CHANNEL* 
-𓄂 *_https://whatsapp.com/channel/0029VaihcQv84Om8LP59fO3f_*
+𓄂 *_https://whatsapp.com/channel/0029VaX6kguGk1Fr135z610I_*
 
 > 2nd STEP
 *YOU MUST JOIN OUR GROUP FOR UPDATES ON THIS BOT*
-𓄂 *_https://chat.whatsapp.com/Iu3UYEx1lE6IsHVRU7XKvp_*
+𓄂 *_https://chat.whatsapp.com/LcMZEQJA1ps7w0oXP3NzHt_*
 
 > 3rd STEP
 *FORK AND STAR 🌟 THE REPO FOR COURAGE*
-𓄂 *_https://github.com/Fred1e/lucky_md_*
+𓄂 *_https://github.com/Investor45/kentech_md_*
 
 > 4th STEP 
 *VISIT FOR TUTORIALS IF YOU DON'T KNOW HOW TO DEPLOY*
 𓄂 *_https://www.youtube.com/@freeonlinetvT1_*
 
-*DEVELOPER1: FREDIE TECH*
-𓄂 *_https://wa.link/4d9cpj_*
+*DEVELOPER1: KEN TECH*
+𓄂 *_https://wa.me/237670217260_*
 
 *DEVELOPER2:FREDIE_TECH*
-𓄂 *_https://wa.me/255620814108_*
+𓄂 *_https://wa.me/255752593977*
 
 > MADE ON EARTH BY FREDIE🇹🇿
-> CREATED BY FREDIE TECH💎 `
+> CREATED BY KEN TECH💎 `
 					
 	 await Qr_Code_By_Maher_Zubair.sendMessage(Qr_Code_By_Maher_Zubair.user.id,{text:SIGMA_MD_TEXT},{quoted:session})
 
